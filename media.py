c504fd373098a836a1608d7f3d3533adc2bafae4
VidalRM <95284726+VidalRM@users.noreply.github.com>
2023-05-19 12:54:49 +0200

    Update media.py

--- media.py
+++ media.py
@@ -3,8 +3,3 @@ def calcular_media(valores):
     cantidad_valores = len(valores)
     media = suma / cantidad_valores
     return media
-
-# Ejemplo de uso
-valores = [5, 2, 9, 1, 7, 6, 3]
-media = calcular_media(valores)
-print("La media de la lista de valores es:", media)
